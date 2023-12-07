94875dc8ba7e6d7e9fba7187dfb0cdada227df1e
Elise Ngo <elisetvy@gmail.com>
2023-12-06 21:16:02 -0800

    Format landing

--- src/About.jsx
+++ src/About.jsx
@@ -2,8 +2,8 @@ function About() {
 
     return (
         <div className="h-full bg-white rounded-t-xl overflow-hidden">
-            <img className="w-full h-1/2 object-cover" src="https://i.pinimg.com/1200x/24/1f/49/241f49ca612ef379a78fdcf7b8471ada.jpg" alt="cat" />
-            <div className="h-1/2 flex flex-col justify-center">
+            <img className="w-full h-2/5 object-cover" src="https://i.pinimg.com/1200x/24/1f/49/241f49ca612ef379a78fdcf7b8471ada.jpg" alt="cat" />
+            <div className="h-3/5 flex flex-col justify-center">
                 <div className="text-3xl text-center px-6 flex text-center justify-center items-center gap-4">
                     <div className="h-full flex flex-col items-end gap-4">
                         <div className="border border-black w-[6px] rotate-[30deg] flex"></div>
@@ -18,7 +18,7 @@ function About() {
                     </div>
                 </div>
                 <div className="px-4 py-4 text-center">I&apos;m Elise, a Full Stack Software Engineer with a background in Language Studies and Marketing.</div>
-                <div className="px-4 text-center text-sm"><b>Interests: </b> reading, crocheting, spitting bars in several languages (natural and programming), traveling</div>
+                <div className="px-4 text-center"><b>Interests: </b> reading, crocheting, spitting bars in several languages (natural and programming), collaging, traveling</div>
             </div>
         </div>
     )
--- src/Landing.jsx
+++ src/Landing.jsx
@@ -3,8 +3,12 @@ import Collage from "./assets/Collage.png"
 function Landing() {
 
     return (
-        <div className="h-full flex flex-col justify-center items-center">
-            <img className="w-full h-full object-cover" src={Collage} alt="collage" />
+        <div className="relative h-full flex flex-col justify-center items-center">
+            <div className="absolute -top-1 -left-1 w-[10px] h-[10px] rounded-full bg-yellow border border-black"></div>
+            <div className="absolute -top-1 -right-1 w-[10px] h-[10px] rounded-full bg-yellow border border-black"></div>
+            <div className="absolute -bottom-1 -left-1 w-[10px] h-[10px] rounded-full bg-yellow border border-black"></div>
+            <div className="absolute -bottom-1 -right-1 w-[10px] h-[10px] rounded-full bg-yellow border border-black"></div>
+            <img className="border border-black w-full h-full object-cover" src={Collage} alt="collage" />
         </div>
     )
 }
