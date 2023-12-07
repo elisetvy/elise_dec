0316779dc80446155f32ed1f37343458ae5c331c
Elise Ngo <elisetvy@gmail.com>
2023-12-06 18:51:00 -0800

    Add Nunito Sans font

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 function Nav() {
 
     return (
-        <div className="flex justify-center gap-10 pb-4">
-            <Link to="/about">About</Link>
-            <Link to="/experience">Experience</Link>
-            <Link to="/contact">Contact</Link>
+        <div className="flex justify-center text-center pb-4">
+            <Link className="w-1/3" to="/about">About</Link>
+            <Link className="w-1/3" to="/experience">Experience</Link>
+            <Link className="w-1/3" to="/contact">Contact</Link>
         </div>
     )
 }
