8fe3df2b120451e6b873328f56107800f5543c87
Elise Ngo <elisetvy@gmail.com>
2023-12-12 16:49:19 -0800

    Add new landing img

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,15 +1,15 @@
-import Collage from "./assets/Collage.webp"
+import Geometry from "./assets/Geometry.webp"
 
 function Landing() {
 
     return (
-        <div className="max-h-[600px] flex flex-col justify-center items-center">
-            <div className="relative max-h-full">
+        <div className="h-full flex flex-col justify-center items-center">
+            <div className="relative h-full">
                 <div className="absolute -top-1 -left-1 w-[10px] h-[10px] rounded-full bg-yellow border border-[#010602]"></div>
                 <div className="absolute -top-1 -right-1 w-[10px] h-[10px] rounded-full bg-yellow border border-[#010602]"></div>
                 <div className="absolute -bottom-1 -left-1 w-[10px] h-[10px] rounded-full bg-yellow border border-[#010602]"></div>
                 <div className="absolute -bottom-1 -right-1 w-[10px] h-[10px] rounded-full bg-yellow border border-[#010602]"></div>
-                <img className="border border-[#010602] w-full h-full object-contain" src={Collage} alt="collage" />
+                <img className="border border-[#010602] w-full h-full object-cover" src={Geometry} alt="collage" />
             </div>
         </div>
     )
